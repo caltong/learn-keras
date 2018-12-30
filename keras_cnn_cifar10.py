8e14a4fdf0d3a6815e27e20650200fd4bfc264e2
Cal Tong <caltong1027@gmail.com>
2018-12-30 12:00:04 +0800

    add test data

--- keras_cnn_cifar10.py
+++ keras_cnn_cifar10.py
@@ -30,13 +30,13 @@ def get_photo_rgb(pixel):
 
 x_train = []
 y_train = []
-for i in (1, 2, 3, 4, 5):
-    path = 'cifar-10-batches-py\data_batch_' + str(i)
+for i in [1, 2, 3, 4, 5]:
+    path = 'cifar-10-batches-py/data_batch_' + str(i)
     data = unpickle(path)
     for i in range(10000):
         x_train.append(data[b'data'][i])  #把所有数据读入x_train和y_train
         y_train.append(data[b'labels'][i])
-print(len(x_train), len(y_train))
+print('train_data shape\n',len(x_train), len(y_train))
 # 转换RBG成三个通道
 x_train_rgb = np.zeros((50000, 3, 1024), dtype=np.float32)
 for i in range(50000):
@@ -53,11 +53,38 @@ for i in range(50000):
     x_train_rgb_two_dimension[i][0] = np.array(x_train_rgb[i][0]).reshape(32, 32)
     x_train_rgb_two_dimension[i][1] = np.array(x_train_rgb[i][1]).reshape(32, 32)
     x_train_rgb_two_dimension[i][2] = np.array(x_train_rgb[i][2]).reshape(32, 32)
+
+# test_train
+path = 'cifar-10-batches-py/test_batch'
+data = unpickle(path)                   #打开数据集 获取数据
+print('test_data shape\n',len(data[b'data']))
+
+x_test = []
+y_test = []
+for i in range(10000):
+    x_test.append(data[b'data'][i])     #获取data信息
+    y_test.append(data[b'labels'][i])   #获取data标签
+print(len(x_test),len(x_test[0]))
+
+x_test_rgb = np.zeros((10000,3,1024),dtype=np.float32) #转换成3通道
+for i in range(10000):
+    x_test_rgb[i][0],x_test_rgb[i][1],x_test_rgb[i][2] = get_photo_rgb(x_test[i])  #转换成3通道
+print(x_test_rgb.shape)
+
+x_test_rgb = x_test_rgb/255.0           #0-255 int 转换成 0-1 float32
+y_test = np_utils.to_categorical(y_test,num_classes=10)  # 标准化
+x_test_rgb_two_dimension = np.zeros((10000,3,32,32), dtype=np.float32)  #转换成2维
+for i in range(10000):
+    x_test_rgb_two_dimension[i][0] = np.array(x_test_rgb[i][0]).reshape(32,32)
+    x_test_rgb_two_dimension[i][1] = np.array(x_test_rgb[i][1]).reshape(32,32)
+    x_test_rgb_two_dimension[i][2] = np.array(x_test_rgb[i][2]).reshape(32,32)
+print(x_test_rgb_two_dimension.shape,y_test.shape)
+
+# model
 model = Sequential()
 model.add(Conv2D(
     32,
     kernel_size=(3, 3),
-
     data_format='channels_first',
 ))
 model.add(Activation('relu'))
@@ -96,4 +123,7 @@ tbCallBack = TensorBoard(log_dir='./logs',  # log 目录
                  embeddings_layer_names=None,
                  embeddings_metadata=None)
 
-model.fit(x_train_rgb_two_dimension, y_train, epochs=12, batch_size=128, callbacks=[tbCallBack])
+model.fit(x_train_rgb_two_dimension, y_train, epochs=3, batch_size=128, callbacks=[tbCallBack])
+score = model.evaluate(x_test_rgb_two_dimension,y_test,verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
